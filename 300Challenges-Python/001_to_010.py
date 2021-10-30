3a319ef4706512755ceb579aa8c5041e105b73d6
goshdding <goshdding@gmail.com>
2021-10-31 00:07:35 +0900

    Update 001_to_010.py
    001 to 008
    211031

--- 300Challenges-Python/001_to_010.py
+++ 300Challenges-Python/001_to_010.py
@@ -1,1 +1,32 @@
+# 초보자를 위한 파이썬 300제
+# 001 ~ 010
+
+# 001
+print("Hello World")
+
+# 002
+print("Mary's cosmetics")
+
+# 003
+print('Mr.Shin yelled "Thief!".')
+
+# 004
+print("C:\Windows")
+
+# 005
+print("안녕하세요. \n만나서\t\t반갑습니다.")
+# \n == 줄바꿈
+# \t == tab
+
+# 006
+print("오늘은", "월요일") # 오늘은 월요일
+
+# 007
+# print("naver;kakao;sk;samsung")
+print("naver", "kakao", "sk", "samsung", sep=";") # sep="구분기호"
+
+# 008
+print("naver", "kakao", "sk", "samsung", sep="/")
+
+
 
